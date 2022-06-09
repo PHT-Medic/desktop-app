22fac3e0953b2ca95476989dae162d4b1d1a8515
tada5hi <peter.placzek1996@gmail.com>
2022-06-09 14:14:21 +0200

    fix big-int conversion

--- src/renderer/domains/encryption/utils/paillier.ts
+++ src/renderer/domains/encryption/utils/paillier.ts
@@ -8,26 +8,33 @@
 import { PrivateKey } from 'paillier-bigint';
 import { TarFile } from '../../train-result/type';
 
-function decryptContentRecursive(privateKey: PrivateKey, data: any) {
-    switch (true) {
-        case Object.prototype.toString.call(data) === '[object Object]': {
-            const keys = Object.keys(data);
-            for (let i = 0; i < keys.length; i++) {
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                data[keys[i]] = decryptContentRecursive(privateKey, data[keys[i]]);
-            }
-
-            return data;
+function decryptContentRecursive(privateKey: PrivateKey, data: unknown) {
+    if(
+        typeof data === 'object' &&
+        data !== null
+    ) {
+        const keys = Object.keys(data);
+        for (let i = 0; i < keys.length; i++) {
+            data[keys[i]] = decryptContentRecursive(privateKey, data[keys[i]]);
         }
-        default:
-            return privateKey.decrypt(BigInt(data as string));
+
+        return data;
     }
+
+    if(typeof data === 'number') {
+        return privateKey.decrypt(BigInt(data));
+    }
+
+    return data;
 }
 
 export function decryptPaillierNumberInTarFiles(
     privateKey: PrivateKey,
     files: TarFile[],
 ) {
+    // @ts-ignore
+    BigInt.prototype.toJSON = function() { return this.toString() }
+
     for (let i = 0; i < files.length; i++) {
         if (files[i].path.indexOf('.he.json') === -1) {
             // eslint-disable-next-line no-continue
@@ -35,8 +42,9 @@ export function decryptPaillierNumberInTarFiles(
         }
 
         const parsed = JSON.parse(files[i].content.toString());
+        const decrypted = decryptContentRecursive(privateKey, parsed);
 
-        files[i].content = Buffer.from(JSON.stringify(decryptContentRecursive(privateKey, parsed)));
+        files[i].content = Buffer.from(JSON.stringify(decrypted));
     }
 
     return files;

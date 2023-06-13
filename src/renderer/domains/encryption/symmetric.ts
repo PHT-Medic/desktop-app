94007e2d1d999e98159eef3327fd70f3208692e4
davidhieber <david.hieber@uni-tuebingen.de>
2023-06-13 13:58:01 +0200

    remove toString() from decrypted result, -> createts problems for not string result files

--- src/renderer/domains/encryption/symmetric.ts
+++ src/renderer/domains/encryption/symmetric.ts
@@ -5,9 +5,8 @@ export function decryptSymmetric(key: Buffer, data: Buffer, ivLength = 16): stri
     const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
     decipher.write(data.slice(ivLength));
     decipher.end();
-
-    const decrypted = decipher.read();
-    return decrypted.toString();
+    
+    return decipher.read();
 }
 
 export function encryptSymmetric(key: Buffer, iv: Buffer, data: string) : Buffer {
